91b5ab4f59debfe17c19c9ab88a20337c91e308a
juliah90 <137844881+juliah90@users.noreply.github.com>
2024-05-03 19:30:40 -0700

    updated profile design

--- frontend/src/components/ProfilePage.jsx
+++ frontend/src/components/ProfilePage.jsx
@@ -1,21 +1,27 @@
 import React from 'react';
 import '../styles/profile.css';
+import '../assets/edit-pencil.svg'
 
 const UserProfile = () => {
   return (
-    <div className="profileContainer">
-      <div className="profilePictureAndName">
-        <div className="profilePicture"></div>
-        <div className="profileInfo">
-          <h1 className="profileName">Jane Doe</h1>
-          <h2 className="companyPosition">General Merchandiser</h2>
-          <p className="editProfile">Edit Profile</p>
-        </div>
-      </div>
-      <div className="aboutMeContainer">
-        <h1 className='aboutMe'>About Me</h1>
-        </div>
-    </div>
+    <div className="profileWrapper">
+                <div className="profile">
+                    <div className="profilePicture">
+                        <img src="user-profile-image.jpg" alt="Profile" className="profileImage" />
+                        <a href="#" className="editProfileLink">
+                            <img src={'./assets/edit-pencil.svg'} alt="Edit Profile" className="editProfileIcon" />
+                        </a>
+                    </div>
+                    <div className="profileInfo">
+                        <h1 className="profileName">Jane Doe</h1>
+                        <h2 className="companyPosition">General Merchandiser</h2>
+                    </div>
+                </div>
+                <div className="aboutMe">
+                    <h3>About Me:</h3>
+                    <textarea className="aboutMeInput" placeholder="Write something about yourself..."></textarea>
+                </div>
+            </div>
   );
 };
 
